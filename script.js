fc4df8d5a19eaf09dc12837698a6fcb867452a95
Jaimitra <141642600+Jaimitra@users.noreply.github.com>
2023-11-20 20:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function changeTo() {
+  let para = document.querySelector("p");
+  para.remove();
+  let h1 = document.createElement("h1");
+  h1.innerText = "Entered Metaverse";
+  let body = document.querySelector("body");
+  let btn = document.querySelector("button");
+  body.insertBefore(h1, btn);
+}
